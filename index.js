8b6e76bcefd9669ab288a84d82698967d62b0073
klaudiapiasecka93 <94700459+klaudiapiasecka93@users.noreply.github.com>
2021-11-24 17:33:53 +0100

    Add files via upload
    changed api KEy

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function displayWeatherCondition(response) {
 }
 
 function searchCity(city) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "0ce93978b429c3703475e83611948cc1";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
